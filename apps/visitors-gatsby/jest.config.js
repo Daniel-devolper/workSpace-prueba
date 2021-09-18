8e1db716f0b0878172b3500ac8ed44d1e8f8c522
Daniel-devolper <danielsanchezjv@unimagdalena.edu.co>
2021-09-18 16:05:07 -0500

    firs-commit

---
+++ apps/visitors-gatsby/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  displayName: 'visitors-gatsby',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/visitors-gatsby',
+};
